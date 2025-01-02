e96a3c3c4d2dfad15fd9700a64c437155cd193e9
Maximilian <scientificresearch309@gmail.com>
2025-01-02 20:21:48 +0100

    refactor image loading logic to remove unnecessary variable declarations and enhance clarity

--- script.js
+++ script.js
@@ -52,25 +52,27 @@ let currentImg; // we need a global variable here!
 (async () => {
   try {
     // NOTE: we don't need to define different img1,img2,img3 and also currentImg variables because we are only in one block which is try{} catch block(). When we use .then() we are in several blocks, that's why we have to use different varibales and a global variable to make a connection between all these variables!
-    let img = await createImage("./img/img-1.jpg");
+
     // currentImg = img1; // to hide it in the next step!
+    // currentImg = img2; // to hide it in the next step!
+    // currentImg = img3; // to hide it in the next step!
+
+    // Load image 1
+    let img = await createImage("./img/img-1.jpg");
     console.log("Image 1 loaded!");
     await wait(2);
-
     img.style.display = "none"; // hide the first image
-    img = await createImage("./img/img-2.jpg"); // return a new promise which is img and
 
-    // currentImg = img2; // to hide it in the next step!
+    // Load image 2
+    img = await createImage("./img/img-2.jpg"); // return a new promise which is img and
     console.log("Image 2 loaded!");
     await wait(2);
-
     img.style.display = "none"; // hide the second image
-    img = await createImage("./img/img-3.jpg");
 
-    // currentImg = img3; // to hide it in the next step!
+    // Load image 3
+    img = await createImage("./img/img-3.jpg");
     console.log("Image 3 loaded!");
     await wait(2);
-
     img.style.display = "none"; // hide the third image
   } catch (err) {
     // console.error(err.message);
