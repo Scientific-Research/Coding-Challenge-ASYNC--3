877802df9c0e62c7c53729dee29b045e12638d88
Maximilian <scientificresearch309@gmail.com>
2025-01-02 16:08:13 +0100

    refactor loadNPause function to use async/await for image loading and error handling

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ PART 1
 write an async function 'loadNPause' that recreates Coding-Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more. Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Netwrok tab!
 */
 
-const loadNPause = (imgPath) => {
+const createImage = (imgPath) => {
   return new Promise((resolve, reject) => {
     const img = document.createElement("img");
 
@@ -45,38 +45,68 @@ const wait = (seconds) => {
 };
 
 //////////// CHAT GPT ANSWER ///////////////////////
+// let currentImg; // we need a global variable here!
+
+// createImage("./img/img-1.jpg")
+//   .then((img) => {
+//     currentImg = img; // to hide it in the next step!
+//     console.log("Image 1 loaded!");
+//     return wait(2);
+//   })
+//   // wait() doesn't resolve anything, it means it doesn't return anything, therefore, we don't have any parameter for callback function in then() below!
+//   .then(() => {
+//     // We don't have any access to the img parameter here, that's why we use a global variable for that => currentImg
+//     currentImg.style.display = "none"; // hide the first image
+//     return createImage("./img/img-2.jpg"); // return a new promise which is img and we receive this img below at the next then()!
+//   })
+//   .then((img) => {
+//     currentImg = img; // to hide it in the next step!
+//     console.log("Image 2 loaded!");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none"; // hide the second image
+//     return createImage("./img/img-3.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img; // to hide it in the next step!
+//     console.log("Image 3 loaded!");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none"; // hide the third image
+//   })
+//   .catch((err) => {
+//     console.error("Error loading image:", err.message);
+//   });
+//////////// CHAT GPT ANSWER ///////////////////////
+
 let currentImg; // we need a global variable here!
 
-loadNPause("./img/img-1.jpg")
-  .then((img) => {
-    currentImg = img; // to hide it in the next step!
+const loadNPause = async () => {
+  try {
+    const img1 = await createImage("./img/img-1.jpg");
+    currentImg = img1; // to hide it in the next step!
     console.log("Image 1 loaded!");
-    return wait(2);
-  })
-  // wait() doesn't resolve anything, it means it doesn't return anything, therefore, we don't have any parameter for callback function in then() below!
-  .then(() => {
-    // We don't have any access to the img parameter here, that's why we use a global variable for that => currentImg
+    wait(2);
+
     currentImg.style.display = "none"; // hide the first image
-    return loadNPause("./img/img-2.jpg"); // return a new promise which is img and we receive this img below at the next then()!
-  })
-  .then((img) => {
-    currentImg = img; // to hide it in the next step!
+    const img2 = await createImage("./img/img-2.jpg"); // return a new promise which is img and
+    currentImg = img2; // to hide it in the next step!
     console.log("Image 2 loaded!");
-    return wait(2);
-  })
-  .then(() => {
+    wait(2);
+
     currentImg.style.display = "none"; // hide the second image
-    return loadNPause("./img/img-3.jpg");
-  })
-  .then((img) => {
-    currentImg = img; // to hide it in the next step!
+    const img3 = await createImage("./img/img-3.jpg");
+
+    currentImg = img3; // to hide it in the next step!
     console.log("Image 3 loaded!");
-    return wait(2);
-  })
-  .then(() => {
+    wait(2);
+
     currentImg.style.display = "none"; // hide the third image
-  })
-  .catch((err) => {
-    console.error("Error loading image:", err.message);
-  });
-//////////// CHAT GPT ANSWER ///////////////////////
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+loadNPause();
